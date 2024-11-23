7c4a19abe3da795e2e1f01f815e0c93d9c846a4f
countingbeeps <gamedungeon@protonmail.com>
2024-11-22 21:28:27 -0600

    maintainers: add gamedungeon

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7798,6 +7798,11 @@
     githubId = 293586;
     name = "Adam Gamble";
   };
+  gamedungeon = {
+    github = "CountingBeeps";
+    githubId = 60719255;
+    name = "gamedungeon";
+  };
   gangaram = {
     email = "Ganga.Ram@tii.ae";
     github = "gangaram-tii";
